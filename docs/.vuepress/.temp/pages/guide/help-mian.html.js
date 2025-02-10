e3b55c4a98c665dc2453e87c18c12aa76e5e1cba
banchen21 <banchen19@outlook.com>
2025-02-11 01:40:44 +0800

    fix

--- docs/.vuepress/.temp/pages/guide/help-mian.html.js
+++ docs/.vuepress/.temp/pages/guide/help-mian.html.js
@@ -1,3 +1,3 @@
 import comp from "E:/mc-doc/docs/.vuepress/.temp/pages/guide/help-mian.html.vue"
-const data = JSON.parse("{\"path\":\"/guide/help-mian.html\",\"title\":\"🚀 入门指南\",\"lang\":\"cn-ZH\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"guide/help-mian.md\"}")
+const data = JSON.parse("{\"path\":\"/guide/help-mian.html\",\"title\":\"🚀 入门指南\",\"lang\":\"cn-ZH\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1739208795000,\"contributors\":[{\"name\":\"banchen21\",\"username\":\"banchen21\",\"email\":\"banchen19@outlook.com\",\"commits\":1,\"url\":\"https://github.com/banchen21\"}]},\"filePathRelative\":\"guide/help-mian.md\"}")
 export { comp, data }
